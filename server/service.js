49614a3394a143ae02221cac50d2ec9160d9e9fb
Angelo Fernandes <afbo2003@gmail.com>
2022-03-14 20:59:48 -0300

    aula-01 implementacao inicial backend e 100% de cobertura para routes

---
+++ server/service.js
@@ -0,0 +1,36 @@
+import fs from 'fs';
+import fsPromises from 'fs/promises';
+import { join, extname } from 'path';
+import config from './config.js';
+
+const {
+  dir: { publicDir },
+} = config;
+
+export class Service {
+
+  createFileStream(filename) {
+    return fs.createReadStream(filename);
+  }
+
+  async getFileInfo(file) {
+    //home/index.html
+    const fullFilePath = join(publicDir, file);
+    // valida se existe
+    await fsPromises.access(fullFilePath);
+    const fileType = extname(fullFilePath);
+
+    return {
+      type: fileType,
+      name: fullFilePath,
+    };
+  }
+
+  async getFileStream(file) {
+    const { name, type } = await this.getFileInfo(file);
+    return {
+      stream: this.createFileStream(name),
+      type,
+    };
+  }
+}
---
+++ tests/unit/_util/testUtil.js
@@ -0,0 +1,52 @@
+import { jest } from '@jest/globals';
+import { Readable, Writable } from 'stream';
+
+export default class TestUtil {
+  // Simulando uma entrega de dados via stream
+  static generateReadableStream(data) {
+    return new Readable({
+      read() {
+        for (const item of data) {
+          this.push(item);
+        }
+
+        this.push(null);
+      },
+    });
+  }
+
+  // Simulando o consumo de dados via stream
+  static generateWritableStream(onData) {
+    return new Writable({
+      write(chunk, enc, callBack) {
+        onData(chunk);
+
+        callBack(null, chunk);
+      },
+    });
+  }
+
+  static defaultHandleParams() {
+    const requestStream = TestUtil.generateReadableStream([
+      'Body da requisicao',
+    ]);
+    const responseStream = TestUtil.generateWritableStream(() => {});
+
+    const data = {
+      request: Object.assign(requestStream, {
+        headers: {},
+        method: '',
+        url: '',
+      }),
+      response: Object.assign(responseStream, {
+        writeHead: jest.fn(),
+        end: jest.fn(),
+      }),
+    };
+
+    return {
+      values: () => Object.values(data),
+      ...data,
+    };
+  }
+}
